1a0dd24f9117233cbe38a26fb115cb6075fe9c54
Marie / 정민지 <44185091+selene4515@users.noreply.github.com>
2023-01-18 16:31:06 +0900

    [Gold IV] Title: 세 수의 합, Time: 348 ms, Memory: 31652 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.js
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.js
@@ -8,28 +8,9 @@ let [N, ...arr] = fs
   .split("\n")
   .map(Number);
 
-let two = [];
-for (let i = 0; i < N; i++) {
-  for (let j = 0; j < N; j++) {
-    two.push(arr[i] + arr[j]);
-  }
-}
-two.sort((a, b) => a - b);
-
-let ans = [];
-for (let i = N - 1; i > 0; i--) {
-  for (let j = 0; j < i; j++) {
-    let a = binarySearch(two.length, arr[i] - arr[j]);
-    if (a >= 0) {
-      ans.push(arr[i]);
-    }
-  }
-}
-console.log(Math.max(...ans));
-
-function binarySearch(n, target) {
+let binarySearch = (target) => {
   let st = 0;
-  let en = n - 1;
+  let en = two.length - 1;
 
   while (st <= en) {
     let mid = Math.floor((st + en) / 2);
@@ -42,4 +23,22 @@ function binarySearch(n, target) {
     }
   }
   return -1;
+};
+arr.sort((a, b) => a - b);
+
+let two = [];
+for (let i = 0; i < N; i++) {
+  for (let j = i; j < N; j++) {
+    two.push(arr[i] + arr[j]);
+  }
+}
+two.sort((a, b) => a - b);
+
+for (let i = N - 1; i > 0; i--) {
+  for (let j = 0; j < i; j++) {
+    if (binarySearch(arr[i] - arr[j]) >= 0) {
+      console.log(arr[i]);
+      return;
+    }
+  }
 }
